ea39ed958f6ba22bf97abc4f607f30020e3de86c
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-05-03 12:52:28 -0400

    api route working now

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,8 +1,12 @@
 const db = require('../db/db.json');
 const fs = require('fs');
 
+
+
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        res.json(db);
-    })
+        res.json(db)
+    
+    });
+
 }
--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
-
 const PORT = process.env.PORT || 7000;
+const notes = require('../Develop/db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
+app.get("/api/notes", function(req, res) {
+  return res.json(notes);
+});
 
 
 
